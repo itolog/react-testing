6d2b9970bffa9ae1d06a82b20e64b1a699023bc4
itolog <paukanlove@gmail.com>
2023-01-27 14:33:30 +0200

    first commit

---
+++ craco.config.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
+const resolvePath = (p) => {
+  return path.resolve(__dirname, p);
+};
+
+module.exports = {
+  typescript: {
+    enableTypeChecking: true,
+  },
+  webpack: {
+    alias: {
+      "@components": resolvePath("src/components"),
+      "@images": resolvePath("src/components"),
+      "@shared": resolvePath("src/shared"),
+      "@hooks": resolvePath("src/shared/hooks"),
+      "@store": resolvePath("src/store"),
+    },
+  },
+};
